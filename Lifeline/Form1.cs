3214cd046b6fe5b5f552b40d0f798a51287ed212
K.Priya <krishnapriyagarimella9@gmail.com>
2021-12-12 10:16:59 +0530

    added user table

--- Lifeline/Form1.cs
+++ Lifeline/Form1.cs
@@ -16,5 +16,37 @@ namespace Lifeline
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String uname, pwd;
+            uname = textBox1.Text;
+            pwd = textBox2.Text;
+            if(uname.Equals("root") && pwd.Equals("toor"))
+            {
+                label5.Hide();
+                MessageBox.Show("Login successful");
+            }
+            else
+            {
+                label5.Text = "Invalid User Name or Password!";
+                label5.Show();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
